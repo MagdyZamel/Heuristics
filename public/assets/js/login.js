252e4055b0d0f57ed5d92bc2708e7f1023f6a45b
Unknown <esraa.atef9919@gmail.com>
2018-04-28 02:13:04 +0200

    create Signup modal and forget password

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -3,13 +3,13 @@ firebase.auth().onAuthStateChanged(function(user) {
     // User is signed in.
 	  var user = firebase.auth().currentUser;
 	  if(user != null){
-		  window.alert("hhgjhgjjhg");
+		  window.alert("Ahln bik f el-lala land xD");
 	  }
   } else {
     // No user is signed in.
   }
 });
-
+//login
 jQuery(document).ready(function ($) {
   "use strict";
   $('form.modalForm').submit(function () {
@@ -28,5 +28,28 @@ jQuery(document).ready(function ($) {
 	  console.log(error);
 	});
 	return false;
+	});
+});
+// signup
+jQuery(document).ready(function ($) {
+	"use strict"
+	$('form.modalForm').submit(function () {
+		var nameFirst =$('#demo-1').find("#modalName").val();
+		var userName =$('#demo-1').find("#modalusername").val();
+		var mail =$('#demo-1').find("#modalEmail").val();
+		var pass =$('#demo-1').find("#modalpass").val();
+		firebase.auth().createUserWithEmailAndPassword(nameFirst, userName, mail, pass).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+			if (errorCode === 'auth/wrong-password') {
+		alert('Wrong password.');
+	  } else {
+		alert(errorMessage);
+	  }
+	  console.log(error);
+  // ...
+});
+		return false;
 	});
 });
